2101b245cf7d45049be9453196f359b22ef8d758
Norayr <asoyannorayr99@gmail.com>
2023-10-23 20:39:16 +0300

    add commit

--- src/Task1_3_8.java
+++ src/Task1_3_8.java
@@ -1,4 +1,8 @@
 public class Task1_3_8 {
+    public static void main(String[] args) {
+        isPowerOfTwo2(1);
+        System.out.println(isPowerOfTwo2(1));
+    }
     public static boolean isPowerOfTwo2(int a) {
 
         return (a > 0) && (Integer.bitCount(a) == 1);
