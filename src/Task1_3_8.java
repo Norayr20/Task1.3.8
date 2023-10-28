9be565da513719add8d5b1c124b9e8423f59ab37
Norayr <asoyannorayr99@gmail.com>
2023-10-28 17:42:33 +0300

    add

--- src/Task1_3_8.java
+++ src/Task1_3_8.java
@@ -1,10 +1,10 @@
 public class Task1_3_8 {
     public static void main(String[] args) {
-        isPowerOfTwo2(1);
-        System.out.println(isPowerOfTwo2(1));
+        isPowerOfTwo2(-1);
+        System.out.println(isPowerOfTwo2(-1));
     }
     public static boolean isPowerOfTwo2(int a) {
 
-        return (a > 0) && (Integer.bitCount(a) == 1);
+        return (a < 0) && (Integer.bitCount(a) == -1);
     }
 }
